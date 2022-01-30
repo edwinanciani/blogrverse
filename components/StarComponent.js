dc85a840bb081928d5d2638643568873c5ffef76
Edwin Anciani <edwinanciani@gmail.com>
2022-01-30 16:13:24 -0600

    star component

--- components/StarComponent.js
+++ components/StarComponent.js
@@ -1,7 +1,43 @@
-const StarComponent = ({postRef}) => {
+import { Button, useColorModeValue } from "@chakra-ui/react"
+import { BsHeart, BsHeartFill } from "react-icons/bs"
+import { auth, firestore } from "../lib/firebase"
+import { useDocument } from 'react-firebase-hooks/firestore';
+import { increment } from "firebase/firestore"
+const StarComponent = ({postData}) => {
+  const bg = useColorModeValue("primary.100", "primary.300")
+  const color = useColorModeValue("white", "white")
+  const uid = auth.currentUser.uid;
+  const postRef = firestore.collection('posts').doc(postData.slug)
+  const likeRef = postRef.collection('likes').doc(uid)
+  const [likeDoc] = useDocument(likeRef)
+
+  const addLike = async () => {
+    const batch = firestore.batch()
+    batch.update(postRef, {likeCount: increment(1)})
+    batch.set(likeRef, {uid})
+    await batch.commit()
+  }
+
+  const removeLike = async () => {
+    const batch = firestore.batch()
+
+    batch.update(postRef, {LikeCount: increment(-1)})
+    batch.delete(likeRef)
+
+    await batch.commit()
+  }
+
  return(
    <>
-    Start Button
+    {
+     likeDoc ?
+      <Button bg={bg} _hover={{background: 'primary.300'}} onClick={removeLike} color={color} leftIcon={<BsHeartFill />} _focus={{background: 'primary.300', boxShadow: "0 0 1px 2px #C3376a, 0 1px 1px #C3376a"}}>
+        Liked!
+      </Button> :
+      <Button bg={bg} _hover={{background: 'primary.300'}} color={color} onClick={addLike} leftIcon={<BsHeart />} _focus={{background: 'primary.300', boxShadow: "0 0 1px 2px #C3376a, 0 1px 1px #C3376a"}}>
+        Like!
+      </Button> 
+    }
    </>
  )
 }
