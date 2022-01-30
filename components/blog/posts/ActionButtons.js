9e2cfa58e8671e32edff9d9b8a142a59b9042889
Edwin Anciani <edwinanciani@gmail.com>
2022-01-30 16:13:24 -0600

    star component

--- components/blog/posts/ActionButtons.js
+++ components/blog/posts/ActionButtons.js
@@ -1,22 +1,21 @@
 import { Button,  Stack, useColorModeValue } from '@chakra-ui/react'
 import { BiShare } from 'react-icons/bi'
-import { AiOutlineLike } from 'react-icons/ai'
+import StarComponent from '../../StarComponent'
 import Coffee from '../../Coffee'
 
-const ActionButtons = () => {
+const ActionButtons = ({post}) => {
+
   const bg = useColorModeValue("primary.100", "primary.300")
   const color = useColorModeValue("white", "white")
   return (
-    <><Stack direction="column" spacing={4}>
+    <Stack direction="column" spacing={4}>
+      {/* <StarComponent  postData={post}/> */}
       <Button bg={bg} _hover={{background: 'primary.300'}} color={color} leftIcon={<BiShare />} _focus={{background: 'primary.300', boxShadow: "0 0 1px 2px #C3376a, 0 1px 1px #C3376a"}}>
         Share
       </Button>
-      <Button bg={bg} _hover={{background: 'primary.300'}} color={color} leftIcon={<AiOutlineLike />} _focus={{background: 'primary.300', boxShadow: "0 0 1px 2px #C3376a, 0 1px 1px #C3376a"}}>
-        Tip!
-      </Button>
       <Coffee bg={bg} color={color} />
 
-    </Stack></>
+    </Stack>
   )
 }
 
